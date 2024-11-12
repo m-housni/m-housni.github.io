ccb738aef1aa9d569c0eed53b3ba6af9d680d0d5
m-housni <mohamedhousni1000k@gmail.com>
2024-11-13 00:13:26 +0100

    Update populateData.js

--- populateData.js
+++ populateData.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Populate only title and summary
-        document.querySelector('#title').textContent = data.title;
-        document.querySelector('#summary').textContent = data.summary;
-      })
-      .catch(error => console.error('Error loading JSON data:', error));
-  });
+  const url = `data.json?t=${new Date().getTime()}`; // Adds a unique timestamp
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('#title').textContent = data.title;
+      document.querySelector('#summary').textContent = data.summary;
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
+});
